f00023716a38962081296dde0d4c8c45871dbadd
Aditya Prasetyo <adty404@gmail.com>
2024-06-11 13:28:29 +0700

    Transaction

--- gorm_test.go
+++ gorm_test.go
@@ -128,3 +128,146 @@ func TestBatchInsert(t *testing.T) {
 	assert.Nil(t, response.Error)
 	assert.Equal(t, int64(8), response.RowsAffected)
 }
+
+func TestTransactionSuccess(t *testing.T) {
+	err := db.Transaction(
+		func(tx *gorm.DB) error {
+			err := tx.Create(
+				&User{
+					ID:       "10",
+					Password: "rahasia",
+					Name: Name{
+						FirstName: "User 10",
+					},
+				},
+			).Error
+			err = tx.Create(
+				&User{
+					ID:       "11",
+					Password: "rahasia",
+					Name: Name{
+						FirstName: "User 11",
+					},
+				},
+			).Error
+			err = tx.Create(
+				&User{
+					ID:       "12",
+					Password: "rahasia",
+					Name: Name{
+						FirstName: "User 12",
+					},
+				},
+			).Error
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+	)
+
+	assert.Nil(t, err)
+}
+
+func TestTransactionError(t *testing.T) {
+	err := db.Transaction(
+		func(tx *gorm.DB) error {
+			err := tx.Create(
+				&User{
+					ID:       "14",
+					Password: "rahasia",
+					Name: Name{
+						FirstName: "User 13",
+					},
+				},
+			).Error
+			if err != nil {
+				return err
+			}
+
+			return nil
+		},
+	)
+	err = db.Transaction(
+		func(tx *gorm.DB) error {
+			err := tx.Create(
+				&User{
+					ID:       "12",
+					Password: "rahasia",
+					Name: Name{
+						FirstName: "User 12",
+					},
+				},
+			).Error
+			if err != nil {
+				return err
+			}
+
+			return nil
+		},
+	)
+
+	assert.NotNil(t, err)
+}
+
+func TestManualTransactionSuccess(t *testing.T) {
+	tx := db.Begin()
+	defer tx.Rollback()
+
+	err := tx.Create(
+		&User{
+			ID:       "15",
+			Password: "rahasia",
+			Name: Name{
+				FirstName: "User 15",
+			},
+		},
+	).Error
+	assert.Nil(t, err)
+
+	err = tx.Create(
+		&User{
+			ID:       "16",
+			Password: "rahasia",
+			Name: Name{
+				FirstName: "User 16",
+			},
+		},
+	).Error
+	assert.Nil(t, err)
+
+	if err == nil {
+		tx.Commit()
+	}
+}
+
+func TestManualTransactionError(t *testing.T) {
+	tx := db.Begin()
+	defer tx.Rollback()
+
+	err := tx.Create(
+		&User{
+			ID:       "17",
+			Password: "rahasia",
+			Name: Name{
+				FirstName: "User 17",
+			},
+		},
+	).Error
+	assert.Nil(t, err)
+
+	err = tx.Create(
+		&User{
+			ID:       "16",
+			Password: "rahasia",
+			Name: Name{
+				FirstName: "User 16",
+			},
+		},
+	).Error
+	assert.NotNil(t, err)
+
+	if err == nil {
+		tx.Commit()
+	}
+}
